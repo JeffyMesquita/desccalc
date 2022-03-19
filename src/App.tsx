2e0c71931749dce2a316fc6d84abe444a577b404
Jeferson Mesquita <je_2742@hotmail.com>
2022-03-18 22:09:08 -0300

    calculadora

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import { useEffect, useState } from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,31 +8,79 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Center,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
+export const App = () => {
+  const [value, setValue] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [descValue, setDescValue] = useState(0);
+
+  const handleCalcDesc = () => {
+    let desc = 0;
+    let finalValue = 0;
+
+    if (value <= 100) {
+      desc = value * 0.1;
+      finalValue = value - desc;
+    }
+
+    if (value > 100 && value <= 150) {
+      desc = value * 0.2;
+      finalValue = value - desc;
+    }
+
+    if (value > 150) {
+      desc = value * 0.3;
+      finalValue = value - desc;
+    }
+
+    setDescValue(desc);
+    setTotal(finalValue);
+  };
+
+  useEffect(() => {}, [value]);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Center height={"100vh"}>
+        <Box
+          as="main"
+          textAlign="center"
+          fontSize="xl"
+          w={"600px"}
+          h={"600px"}
+          border={"1px"}
+          borderColor={"red"}
+          borderRadius={"20px"}
+        >
+          <Text>Escreva o valor</Text>
+          <Input
+            type="number"
+            placeholder="Digite o valor"
+            w={"50%"}
+            onChange={(e) => setValue(+e.target.value)}
+          />
+
+          <Button
+            onClick={() => {
+              handleCalcDesc();
+            }}
           >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+            Calcular desconto
+          </Button>
+
+          <Text>Valor descontado</Text>
+          <Text>{descValue}</Text>
+
+          <Text>Valor final</Text>
+          <Text>{total}</Text>
+        </Box>
+      </Center>
+    </ChakraProvider>
+  );
+};
